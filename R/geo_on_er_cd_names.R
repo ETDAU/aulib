f411785318aeb3f8d7d4e33c1399dbe2ac9bed70
suesongfassb <sue.song2@ontario.ca>
2022-10-25 10:56:48 -0400

    appending EST ca info

--- R/geo_on_er_cd_names.R
+++ R/geo_on_er_cd_names.R
@@ -6,13 +6,15 @@
 #' @source <https://www12.statcan.gc.ca/census-recensement/2021/geo/aip-pia/attribute-attribs/index2021-eng.cfm?year=2021>
 #' @format A data frame with 50 unique census divisions and 7 columns:
 #' \describe{
+#' \item{est_region}{EST region name.}
+#' \item{est_region_id}{Uniquely identifies an EST region.}
+#' \item{cdname}{Census division name.}
 #' \item{cduid}{Uniquely identifies a census division.}
 #' \item{cddguid_dridugd}{Dissemination Geography Unique Identifier for 2021 census divisions.}
-#' \item{cdname_drnom}{Census division name.}
+#' \item{cdtype}{Census division type.}
+#' \item{ername_renom}{Economic region name}
 #' \item{eruid_reidu}{Uniquely identifies an economic region.}
 #' \item{erdguid_reidugd}{Dissemination Geography Unique Identifier for 2021 economic regions.}
-#' \item{ername_renom}{Economic region name}
-#' \item{ca_name}{IES Catchment Areas}
 #' }
 #' @examples
 #'   geo_on_er_cd_names
